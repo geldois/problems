9218b3e708a951d56c31372499fb47a92781f31c
Angelito Chagas <chagasangelito@gmail.com>
2026-03-03 00:05:32 -0300

    feat: add solution to cses 1068 weird algorithm

--- contests/cses/001-introductory-problems/001-weird-algorithm/001-weird-algorithm.py
+++ contests/cses/001-introductory-problems/001-weird-algorithm/001-weird-algorithm.py
@@ -1,4 +1,17 @@
-# Problem: Weird Algorithm
-# Source: https://cses.fi/problemset/task/1068
-# Technique(s): simulation
+# https://cses.fi/problemset/task/1068
 
+n = int(input())
+
+while True:
+    print(n, end = " ")
+
+    if n == 1:
+        print()
+
+        break
+
+    if n % 2 == 0:
+        n //= 2
+    else:
+        n = n * 3 + 1
+        
