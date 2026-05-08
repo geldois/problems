e374c603989e7cf50b287e1fc5ff982199d8dc2d
Angelito Chagas <chagasangelito@gmail.com>
2026-05-08 15:57:16 -0300

    feat(core): implement bubble sort with early-exit strategy in C

---
+++ core/sorting/c/BubbleSortEarlyExit.c
@@ -0,0 +1,38 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+void bubble_sort(int *array, size_t size) {
+    if (array == NULL || size <= 1) {
+        return;
+    }
+
+    for (size_t i = 0; i < size - 1; i++) {
+        bool swapped = false;
+
+        for (size_t j = 0; j < size - 1 - i; j++) {
+            int curr = array[j];
+            int next = array[j + 1];
+
+            if (curr > next) {
+                array[j] = next;
+                array[j + 1] = curr;
+
+                swapped = true;
+            }
+        }
+
+        if (swapped == false) {
+            break;
+        }
+    }
+}
+
+void print(int *array, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+}
+
+int main() {}
